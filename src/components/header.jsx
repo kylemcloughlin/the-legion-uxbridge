a7f1cf299ca9caee9b1ce5bca7deabd5803d4bbb
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-10-07 16:07:28 -0400

    intial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AppHead extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        Header 
+      </div>
+    )
+  }
+}
+export default AppHead;
