71f5e8a780c0cc1d95821b3da79b8ad977278844
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-10-08 14:07:02 -0400

    added content from site beginning calender bookings

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -7,7 +7,34 @@ class Contact extends React.Component {
 
   render() {
     return (
-    <p> test contact test</p>
+    <div>
+      <h1>Our Location:</h1>
+        <p>
+  109 Franklin Street,
+  Uxbridge, Ontario
+Canada</p>
+        <h2>Contact Us:</h2>
+        <p>Royal Canadian Legion Branch 170
+  Box 137
+  Uxbridge, Ontario.
+  L9P 1J5
+CANADA</p>
+        <p>Donations to the Legion  can be sent to the mailing address above. Please make cheques payable to: Royal Canadian Legion, Branch 170. If making donations to the Legion Poppy Fund, please make cheques payable: Uxbridge Legion Poppy Fund </p>
+        <h2>Email:</h2>
+        <p>legion170@powergate.ca</p>
+        <h2>Telephone</h2>
+        <p>905.852.5293</p>
+        <h2>Club Room Hours :</h2>
+        <h3>Summer Hours:  June – August</h3>
+        <p>Monday –  Saturday – 2:00 pm – 8:00 pm.
+Some evenings will close later than posted if there is a function going on.
+Sunday – 2:00 pm – 6:00 pm</p>
+        <h3>Winter Hours:  September  –  June</h3>
+        <p>Monday to Friday: 1:00 pm – 9:00 pm.
+Saturday: 1:00 pm – 9:00 pm
+Sunday: 2:00 pm – 6:00 pm</p>
+
+    </div>
     )
   }
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,16 @@ class Home extends React.Component {
 
   render() {
     return (
-      <p>home test home</p>
+    <div>
+        <h1>Welcome to the Uxbridge Legion.</h1>
+    <p>Branch 170 recieved its charter in 1930! Active for almost 85 years, the members have met in various 
+        places around town including the old post office, and the old marketplace, and finally in the late 40's 
+        establishing the location that continues to this day. The original building burned down in 1953, and was 
+        rebuilt. It has since had several renovations. It is notable that most of the work on the legion has been done 
+        by volunteers.</p>
+        <h2>Our MISSION: </h2>
+        <p>Serving veterans and their dependents, promoting Remembrance, and acting in the service of Canada and its communities.</p>
+    </div>
     )
   }
 }
