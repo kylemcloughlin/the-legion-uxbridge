9b5a940b751a9a6378fbcf541f1b4fe1b6eb56cb
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-10-16 10:38:28 -0400

    added funtionality to calender

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-class Calender extends React.Component {
+import ReactLightCalendar from '@lls/react-light-calendar'
+import '@lls/react-light-calendar/dist/index.css'
+
+class Calendar extends React.Component {
   constructor(props) {
     super(props)
+    const date = new Date()
+    const startDate = date.getTime()
+    this.state = {
+      startDate, // Today
+      endDate: new Date(startDate).setDate(date.getDate() + 6) // Today + 6 days
+    }
+    console.log(new Date(this.state.startDate))
   }
 
-  render() {
+  onChange = (startDate, endDate) => {
+  this.setState({ startDate, endDate })
+}
+
+  render = () => {
+    const { startDate, endDate } = this.state
+
     return (
-    <p>
-      test cal test
-    </p>
+   <div>
+     <label>Name</label>
+     <input onChange={(e) => {  console.log("1", e.target.value) }  }/>
+
+        <label>Number</label>
+        <input onChange={(e) => { console.log("2", e.target.value) }}/>
+        <label>Name</label>
+        <input onChange={(e) => { console.log("3", e.target.value) }}/>
+
+
+      <ReactLightCalendar startDate={startDate} endDate={endDate} onChange={this.onChange} range displayTime timeZone="EDT"/>
+   </div>
     )
   }
 }
-export default Calender;
+export default Calendar;
