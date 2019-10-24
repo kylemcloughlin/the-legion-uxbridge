ae31cee1e74b1c1314e70e6f902b7857594e2e5d
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-10-24 16:07:02 -0400

    begin styling for home page

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -6,32 +6,32 @@ class Contact extends React.Component {
   render() {
     return (
     <div>
-      <h1>Our Location:</h1>
-        <p>
-  109 Franklin Street,
-  Uxbridge, Ontario
-Canada</p>
-        <h2>Contact Us:</h2>
-        <p>Royal Canadian Legion Branch 170
-  Box 137
-  Uxbridge, Ontario.
-  L9P 1J5
-CANADA</p>
-        <p>Donations to the Legion  can be sent to the mailing address above. Please make cheques payable to: Royal Canadian Legion, Branch 170. If making donations to the Legion Poppy Fund, please make cheques payable: Uxbridge Legion Poppy Fund </p>
-        <h2>Email:</h2>
-        <p>legion170@powergate.ca</p>
-        <h2>Telephone</h2>
-        <p>905.852.5293</p>
+   
+        <label>Our Location:</label>
+        <span> 109 Franklin Street, Uxbridge, Ontario Canada</span>
+       
+        <label>Mailing Address: </label>
+        <span>Royal Canadian Legion Branch 170 Box 137 Uxbridge, Ontario. L9P 1J5 CANADA</span>
+        
+        <label><i> Donations to the Legion  can be sent to the mailing address above. 
+          Please make cheques payable to:</i></label>
+          <span><b>Royal Canadian Legion, Branch 170.</b></span>
+          
+         <label><i>If making donations to the Legion Poppy Fund, please make cheques payable: </i></label>
+         <span><b>Uxbridge Legion Poppy Fund</b></span>
+       
+        <label>Email Address:</label>
+        <span>legion170@powergate.ca</span>
+        
+        <label>Telephone: </label>
+        <span>905.852.5293</span>
+       <div className='hours-div'>
         <h2>Club Room Hours :</h2>
-        <h3>Summer Hours:  June – August</h3>
-        <p>Monday –  Saturday – 2:00 pm – 8:00 pm.
-Some evenings will close later than posted if there is a function going on.
-Sunday – 2:00 pm – 6:00 pm</p>
-        <h3>Winter Hours:  September  –  June</h3>
-        <p>Monday to Friday: 1:00 pm – 9:00 pm.
-Saturday: 1:00 pm – 9:00 pm
-Sunday: 2:00 pm – 6:00 pm</p>
-
+        <label>Summer Hours:  June – August</label>
+        <span>Monday –  Saturday – 2:00 pm – 8:00 pm. Sunday – 2:00 pm – 6:00 pm</span>
+        <label>Winter Hours:  September  –  June</label>
+        <span>Monday to Friday: 1:00 pm – 9:00 pm. Saturday: 1:00 pm – 9:00 pm Sunday: 2:00 pm – 6:00 pm</span>
+       </div>
     </div>
     )
   }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import legion from './legion-outside.jpg'
 class Home extends React.Component {
 
   render() {
     return (
-    <div>
-        <h1>Welcome to the Uxbridge Legion.</h1>
-    <p>Branch 170 recieved its charter in 1930! Active for almost 85 years, the members have met in various 
-        places around town including the old post office, and the old marketplace, and finally in the late 40's 
-        establishing the location that continues to this day. The original building burned down in 1953, and was 
-        rebuilt. It has since had several renovations. It is notable that most of the work on the legion has been done 
+    <div className="home-container">
+       <div className="welcome-pos">
+        <h1 className="welcome">Welcome to the Uxbridge Legion.</h1>
+          <p className="welcome-div">Branch 170 recieved its charter in 1930! Active for almost 85 years, the members have met in various 
+          places around town including the old post office, and the old marketplace, and finally in the late 40's 
+          establishing the location that continues to this day. The original building burned down in 1953, and was 
+          rebuilt. It has since had several renovations. It is notable that most of the work on the legion has been done 
         by volunteers.</p>
+      </div>
+      <img className="photo" src={legion}/>
+      <div className='mission-pos'>
         <h2>Our MISSION: </h2>
-        <p>Serving veterans and their dependents, promoting Remembrance, and acting in the service of Canada and its communities.</p>
+          <p className="mission-div">Serving veterans and their dependents, promoting Remembrance, and acting in the service of Canada and its communities.</p>
+      </div>
     </div>
     )
   }
